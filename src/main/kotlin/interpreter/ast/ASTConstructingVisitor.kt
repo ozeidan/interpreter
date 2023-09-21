b4a9fad7a6229f61b1ba84e2ea8f5fae0c266b3f
Omar Zeidan <o.zeidan97@gmail.com>
2023-09-21 23:08:19 +0200

    Implement construction of AST from parse tree

---
+++ src/main/kotlin/interpreter/ast/ASTConstructingVisitor.kt
@@ -0,0 +1,84 @@
+package interpreter.ast
+
+import SeqLangBaseVisitor
+
+/*
+Visitor on the SeqLang parse tree that constructs an AST of type ASTNode. The AST is structurally more suited for
+semantic analysis and execution of the program, as it directly represents the semantics of the program without
+storing unneeded intermediate nodes which are artefacts of parsing.
+
+The visitor's implementation is split into multiple classes, each corresponding to a concrete AST node type.
+This is done to avoid casting.
+ */
+class ASTConstructingVisitor : SeqLangBaseVisitor<ASTNode>()  {
+    private val statementNodeConstructingVisitor = StatementNodeConstructingVisitor()
+
+    override fun visitProgram(ctx: SeqLangParser.ProgramContext): ASTNode {
+        return ProgramNode(ctx.stmt().map(statementNodeConstructingVisitor::visit))
+    }
+}
+
+private class StatementNodeConstructingVisitor : SeqLangBaseVisitor<StatementNode>() {
+    private val expressionConstructor = ExpressionNodeConstructingVisitor()
+
+    override fun visitVarDeclaration(ctx: SeqLangParser.VarDeclarationContext): StatementNode {
+        return VariableDeclarationNode(ctx.start.line, ctx.IDENT().text, expressionConstructor.visit(ctx.expr()))
+    }
+
+    override fun visitPrintExpression(ctx: SeqLangParser.PrintExpressionContext): StatementNode {
+        return PrintExpressionNode(ctx.start.line, expressionConstructor.visit(ctx.expr()))
+    }
+
+    override fun visitPrintString(ctx: SeqLangParser.PrintStringContext): StatementNode {
+        val text = ctx.STRING().text
+        val trimmedText = text.substring(1, text.length - 1)
+        return PrintStringNode(ctx.start.line, trimmedText)
+    }
+}
+
+private class ExpressionNodeConstructingVisitor : SeqLangBaseVisitor<ExpressionNode>() {
+    override fun visitAddition(ctx: SeqLangParser.AdditionContext): ExpressionNode {
+        return BinOpNode(BinaryOperator.ADDITION, this.visit(ctx.addExpr()), this.visit(ctx.multExpr()))
+    }
+
+    override fun visitSubtraction(ctx: SeqLangParser.SubtractionContext): ExpressionNode {
+        return BinOpNode(BinaryOperator.SUBTRACTION, this.visit(ctx.addExpr()), this.visit(ctx.multExpr()))
+    }
+
+    override fun visitMultiplication(ctx: SeqLangParser.MultiplicationContext): ExpressionNode {
+        return BinOpNode(BinaryOperator.MULTIPLICATION, this.visit(ctx.multExpr()), this.visit(ctx.powerExpr()))
+    }
+
+    override fun visitDivision(ctx: SeqLangParser.DivisionContext): ExpressionNode {
+        return BinOpNode(BinaryOperator.DIVISION, this.visit(ctx.multExpr()), this.visit(ctx.powerExpr()))
+    }
+
+    override fun visitPower(ctx: SeqLangParser.PowerContext): ExpressionNode {
+        return BinOpNode(BinaryOperator.POWER, this.visit(ctx.baseExpr()), this.visit(ctx.powerExpr()))
+    }
+
+    override fun visitParenthesizedExpr(ctx: SeqLangParser.ParenthesizedExprContext): ExpressionNode {
+        return this.visit(ctx.expr())
+    }
+
+    override fun visitIdentifier(ctx: SeqLangParser.IdentifierContext): ExpressionNode {
+        return VariableAccessNode(ctx.IDENT().text)
+    }
+
+    override fun visitSequence(ctx: SeqLangParser.SequenceContext): ExpressionNode {
+        return SequenceNode(this.visit(ctx.expr(0)), this.visit(ctx.expr(1)))
+    }
+
+    override fun visitNumberLiteral(ctx: SeqLangParser.NumberLiteralContext): ExpressionNode {
+        return NumberLiteralNode(ctx.NUMBER().text.toDouble())
+    }
+
+    override fun visitMapping(ctx: SeqLangParser.MappingContext): ExpressionNode {
+        return MappingNode(this.visit(ctx.expr(0)), UnaryLambda(ctx.IDENT().text, this.visit(ctx.expr(1))))
+    }
+
+    override fun visitReduction(ctx: SeqLangParser.ReductionContext): ExpressionNode {
+        return ReducingNode(this.visit(ctx.expr(0)), this.visit(ctx.expr(1)),
+            BinaryLambda(ctx.IDENT(0).text, ctx.IDENT(1).text, this.visit(ctx.expr(2))))
+    }
+}
---
+++ src/test/kotlin/interpreter/ast/ASTConstructingVisitorTest.kt
@@ -0,0 +1,75 @@
+package interpreter.ast
+
+import SeqLangLexer
+import SeqLangParser
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ASTConstructingVisitorTest {
+    private var toTest = ASTConstructingVisitor()
+
+    @Test
+    fun shouldConstructASTCorrectly() {
+        val testProgram = """
+        var n = 500
+        var seq = map({0, n}, i -> i + 1)
+        var someval = 4 * reduce(seq, 0, x y -> x + y)
+        print "hello"
+        out someval
+        """.trimIndent()
+
+        val expectedAST = ProgramNode(listOf(
+            VariableDeclarationNode(1, "n", NumberLiteralNode(500.0)),
+            VariableDeclarationNode(2, "seq",
+                MappingNode(SequenceNode(NumberLiteralNode(0.0), VariableAccessNode("n")),
+                    UnaryLambda("i", BinOpNode(BinaryOperator.ADDITION, VariableAccessNode("i"),
+                        NumberLiteralNode(1.0))))),
+            VariableDeclarationNode(3, "someval",
+                BinOpNode(BinaryOperator.MULTIPLICATION, NumberLiteralNode(4.0),
+                    ReducingNode(VariableAccessNode("seq"), NumberLiteralNode(0.0),
+                        BinaryLambda("x", "y",
+                            BinOpNode(BinaryOperator.ADDITION, VariableAccessNode("x"), VariableAccessNode("y")))
+                    ))),
+            PrintStringNode(4, "hello"),
+            PrintExpressionNode(5, VariableAccessNode("someval"))
+        ))
+
+        assertGeneratesAST(testProgram, expectedAST)
+    }
+
+    @Test
+    fun shouldConstructComplexExpressionCorrectly() {
+        val testProgram = "out 2 + 3 * 4 ^ 2 + 7"
+        val expectedAST = ProgramNode(listOf(PrintExpressionNode(1,
+            BinOpNode(
+                BinaryOperator.ADDITION,
+                BinOpNode(
+                    BinaryOperator.ADDITION,
+                    NumberLiteralNode(2.0),
+                    BinOpNode(
+                        BinaryOperator.MULTIPLICATION,
+                        NumberLiteralNode(3.0),
+                        BinOpNode(
+                            BinaryOperator.POWER,
+                            NumberLiteralNode(4.0),
+                            NumberLiteralNode(2.0)
+                        )
+                    )
+                ),
+                NumberLiteralNode(7.0)
+        ))))
+
+        assertGeneratesAST(testProgram, expectedAST)
+    }
+
+    private fun assertGeneratesAST(program: String, expected: ASTNode) {
+        val input = CharStreams.fromString(program)
+        val lexer = SeqLangLexer(input)
+        val tokens = CommonTokenStream(lexer)
+        val parser = SeqLangParser(tokens)
+        val tree = parser.program()
+        assertEquals(expected, toTest.visit(tree))
+    }
+}
