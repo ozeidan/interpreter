5fe3934d5e3cd61ba19c95ec0d720494ef991dde
Omar Zeidan <o.zeidan97@gmail.com>
2023-10-04 21:12:10 +0200

    Document Debouncer class

--- src/main/kotlin/gui/Debouncer.kt
+++ src/main/kotlin/gui/Debouncer.kt
@@ -2,13 +2,20 @@ package gui
 
 import javax.swing.Timer;
 
-class Debouncer(private val debounceIntervalMs: Int) {
+/**
+ * Debouncer batches calls to debounce together, running only the runnable that was passed to the latest call
+ * to debounce, after the intervalsMs milliseconds have passed.
+ */
+class Debouncer(private val intervalMs: Int) {
     private var latestRunnable : Runnable? = null
 
+    /**
+     * Runs runnable after intervalMs milliseconds, if no further calls to debounce are made during that time.
+     */
     fun debounce(runnable: Runnable) {
         latestRunnable = runnable
         // no need to synchronize because swing schedules these on the same event dispatch thread
-        val timer = Timer(debounceIntervalMs) {
+        val timer = Timer(intervalMs) {
             if (latestRunnable == runnable) {
                 runnable.run()
             }
